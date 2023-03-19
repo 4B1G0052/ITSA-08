4093705905c6fb79637c8aaa2313205b4078bd23
4B1G0052 <4B1G0052@stust.edu.tw>
2023-03-19 09:47:45 +0800

    加入專案檔案。

---
+++ ITSA 08/ITSA 08.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>  
+using namespace std;
+int main()
+{
+    int n;
+    int primes[100] = { 0 };
+    cin >> n;
+    int i;
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            break;
+        }
+    }
+    if (i == n) {
+        cout<<"YES"<<endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+}
